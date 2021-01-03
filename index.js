30705a466678bcebbe568920b2efcef86a0ef52e
Dyrkov Andrey <webworkdeveloper@gmail.com>
2021-01-03 20:56:11 +0300

    Registre service worker

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document
     .getElementById('button')
     .addEventListener('click', console.log);
+
+addEventListener('load', async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./serviceWorker.js');
+        } catch (err) {
+            console.log(err);
+        }
+    }
+});
